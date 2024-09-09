13cc7a6fe680c538b06f3d7f8c5416178ec560c5
BoondockTaints <wsollers@gmail.com>
2024-09-09 16:17:16 -0400

    Code coverage initial

--- buildIt.sh
+++ buildIt.sh
@@ -33,6 +33,7 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CPP_COMPILER=/usr/bin/clang++ \
+  -DCMAKE_BUILD_TYPE=Debug \
   ../src/CMakeLists.txt
 
 cd "$currentDir"
