8f6f9355ce090998df2f1c29ecf8a5e9908d7f7e
BoondockTaints <wsollers@gmail.com>
2024-10-09 16:04:45 -0400

    Linked stack

--- src/adt/src/stack/stack.h
+++ src/adt/src/stack/stack.h
@@ -2,7 +2,10 @@
 #define STACK_H
 
 #include <cstdlib>
+#include <optional>
 #include <array>
+
+#include "../node/node.h"
 
 namespace Stacks {
 
@@ -11,8 +14,8 @@ class Stack {
 
 public:
 
-virtual T pop() = 0;
-virtual T peek() = 0;
+virtual std::optional<T> pop() = 0;
+virtual std::optional<T> peek() = 0;
 virtual void push(T data) = 0;
 virtual bool isEmpty() = 0;
 virtual size_t getSize() = 0;
@@ -32,16 +35,16 @@ public:
     delete[] stack;
   }
 
-  T pop() override {
+  std::optional<T> pop() override {
     if (isEmpty()) {
-      return T();
+      return std::nullopt;
     }
     return stack[--size];
   }
 
-  T peek() override {
+  std::optional<T> peek() override {
     if (isEmpty()) {
-      return T();
+      return std::nullopt;
     }
     return stack[size - 1];
   }
@@ -66,6 +69,60 @@ private:
   size_t size;
   T *stack {};
 
+};
+
+template <typename T>
+class LinkedStack : public Stack<T> {
+public:
+  LinkedStack() : size(0), head(nullptr) {}
+
+  ~LinkedStack() {
+    while (head != nullptr) {
+      Nodes::SingleLinkNode<T> *temp = head;
+      head = head->getNext();
+      delete temp;
+      temp = nullptr;
+    }
+  }
+
+  std::optional<T> pop() override {
+    if (isEmpty()) {
+      return std::nullopt;
+    }
+    Nodes::SingleLinkNode<T> *temp = head;
+    head = head->getNext();
+    T data = temp->getData();
+    delete temp;
+    temp = nullptr;
+    size--;
+    return data;
+  }
+
+  std::optional<T> peek() override {
+    if (isEmpty()) {
+      return std::nullopt;
+    }
+    return head->getData();
+  }
+
+  void push(T data) override {
+    Nodes::SingleLinkNode<T> *newNode = new Nodes::SingleLinkNode<T>(data);
+    newNode->setNext(head);
+    head = newNode;
+    size++;
+  }
+
+  bool isEmpty() override {
+    return size == 0;
+  }
+
+  size_t getSize() override {
+    return size;
+  }
+
+private:
+  size_t size;
+  Nodes::SingleLinkNode<T> *head;
 };
 
 } // namespace Stacks
--- src/adt/test/stack_tests.cpp
+++ src/adt/test/stack_tests.cpp
@@ -1,5 +1,6 @@
-#include "../src/stack/stack.h"
 #include <gtest/gtest.h>
+
+#include "../src/stack/stack.h"
 
 class SkipFixture : public ::testing::Test {
 protected:
@@ -17,6 +18,18 @@ TEST(StackTest, ConstructAndIsEmpty) {
   Stacks::ArrayStack<int> stack(10);
   EXPECT_TRUE(stack.isEmpty());
   EXPECT_EQ(stack.getSize(), 0);
+}
+
+TEST(StackTest, EmptyPop) {
+  Stacks::ArrayStack<int> stack(10);
+  EXPECT_TRUE(stack.isEmpty());
+  EXPECT_FALSE(stack.pop().has_value());
+}
+
+TEST(StackTest, EmptyPeek) {
+  Stacks::ArrayStack<int> stack(10);
+  EXPECT_TRUE(stack.isEmpty());
+  EXPECT_FALSE(stack.peek().has_value());
 }
 
 TEST(StackTest, TopItemIsPeekable) {
@@ -41,6 +54,56 @@ TEST(StackTest, TopItemIsPopableMultiItems) {
   auto item = stack.pop();
   EXPECT_EQ(item, 1);
 }
+
+
+
+
+
+
+
+
+
+TEST(LinkedStackTest, ConstructAndIsEmpty) {
+  Stacks::LinkedStack<int> stack;
+  EXPECT_TRUE(stack.isEmpty());
+  EXPECT_EQ(stack.getSize(), 0);
+}
+
+TEST(LinkedStackTest, EmptyPop) {
+  Stacks::LinkedStack<int> stack;
+  EXPECT_TRUE(stack.isEmpty());
+  EXPECT_FALSE(stack.pop().has_value());
+}
+
+TEST(LinkedStackTest, EmptyPeek) {
+  Stacks::LinkedStack<int> stack;
+  EXPECT_TRUE(stack.isEmpty());
+  EXPECT_FALSE(stack.peek().has_value());
+}
+
+TEST(LinkedStackTest, TopItemIsPeekable) {
+  Stacks::LinkedStack<int> stack;
+  stack.push(1);
+  EXPECT_EQ(stack.peek(), 1);
+}
+
+TEST(LinkedStackTest, TopItemIsPeekableMultiItems) {
+  Stacks::LinkedStack<int> stack;
+  stack.push(11);
+  stack.push(12);
+  stack.push(1);
+  EXPECT_EQ(stack.peek(), 1);
+}
+
+TEST(LinkedStackTest, TopItemIsPopableMultiItems) {
+  Stacks::LinkedStack<int> stack;
+  stack.push(11);
+  stack.push(12);
+  stack.push(1);
+  auto item = stack.pop();
+  EXPECT_EQ(item, 1);
+}
+
 
 
 
